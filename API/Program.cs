870c83589e6f18eb0b0d91b9124c6b2802396205
Karina Kuroda <karina_kuroda@hotmail.com>
2024-01-19 08:41:45 +0000

    add swagger

--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,23 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using Timesheet.ApplicationServices;
 using Timesheet.ApplicationServices.DTO;
 using Timesheet.ApplicationServices.Interfaces;
 using Timesheet.Data;
 using Timesheet.Domain.Builders;
+using Swashbuckle.AspNetCore.Swagger;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Timesheet API",
+        Description = "Timesheet API"
+    });
+});
+builder.Services.AddControllers();
 builder.Services.AddScoped<IAppointmentService, AppointmentService>();
 builder.Services.AddScoped<IAppointmentRepository, AppointmentRepository>();
 builder.Services.AddScoped<IAppointmentBuilder, AppointmentBuilder>();
@@ -14,13 +26,16 @@ builder.Services.AddScoped<ITimesheetService, TimesheetService>();
 builder.Services.AddScoped<ITimesheetRepository, TimesheetRepository>();
 var connection = builder.Configuration.GetConnectionString("DefaultConnectionPsg");
 builder.Services.AddDbContext<TimesheetContext>(opt => opt.UseNpgsql(connection));
-// builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-var app = builder.Build();
 
+var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
 app.MapGet("/Timesheets", async (ITimesheetService timesheetService) =>
-{
-    var result = await timesheetService.GetAllAsync(new TimesheetFilterDTO());
-    return result == null ? Results.NotFound() : Results.Ok(result);
-});
+    {
+        var result = await timesheetService.GetAllAsync(new TimesheetFilterDTO());
+        return result == null ? Results.NotFound() : Results.Ok(result);
+    }).WithDescription("Some Method Description")
+    .WithOpenApi();
+;
 
 app.Run();
